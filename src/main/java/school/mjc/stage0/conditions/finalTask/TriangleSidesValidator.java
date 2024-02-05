4d46628d3b166d7bc5b2f4d7d3f6fe7769a5e5c4
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 16:31:15 +0500

    Update TriangleSidesValidator.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if (isValidTriangle(firstSide, secondSide, thirdSide)) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
+    }
 
+    private boolean isValidTriangle(double a, double b, double c) {
+        return (a + b > c) && (b + c > a) && (c + a > b);
     }
 }
